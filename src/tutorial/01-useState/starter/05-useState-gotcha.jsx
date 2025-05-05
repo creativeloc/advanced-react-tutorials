e550fd2033f98826df14f3c89a733961b3b5c66e
Lawrence Caulker <locaulker@gmail.com>
2025-05-05 17:58:42 -0400

    useState with function

--- src/tutorial/01-useState/starter/05-useState-gotcha.jsx
+++ src/tutorial/01-useState/starter/05-useState-gotcha.jsx
@@ -1,5 +1,24 @@
-const UseStateGotcha = () => {
-  return <h2>useState "gotcha"</h2>;
-};
+import { useState } from "react"
 
-export default UseStateGotcha;
+const UseStateGotcha = () => {
+  const [value, setValue] = useState(0)
+
+  const handleClick = () => {
+    // setValue(value + 1)
+    setValue((currentState) => {
+      const newState = currentState + 1
+      return newState
+    })
+  }
+
+  return (
+    <div>
+      <h1>{value}</h1>
+      <button type="button" className="btn" onClick={handleClick}>
+        Increase
+      </button>
+    </div>
+  )
+}
+
+export default UseStateGotcha
