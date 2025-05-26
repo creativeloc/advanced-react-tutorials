e8b08f71f13a849d45910713bec9c34636823133
creativeloc <creativeloc@yahoo.com>
2025-05-26 06:13:02 -0400

    adding user to an existing list, and resetting the input name back to empty string

--- src/tutorial/06-forms/starter/02-user-challenge.jsx
+++ src/tutorial/06-forms/starter/02-user-challenge.jsx
@@ -1,21 +1,53 @@
+import { useState } from "react"
+import { data } from "../../../data"
+
 const UserChallenge = () => {
+  const [name, setName] = useState("")
+  const [users, setUsers] = useState(data)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    if (!name) return // early return
+    const fakeId = Date.now()
+    const newUser = { id: fakeId, name: name }
+    const updatedUsers = [...users, newUser]
+    setUsers(updatedUsers)
+    console.log("Form Submitted")
+    setName("")
+  }
+
   return (
     <div>
-      <form className='form'>
+      <form className="form" onSubmit={handleSubmit}>
         <h4>Add User</h4>
-        <div className='form-row'>
-          <label htmlFor='name' className='form-label'>
+        <div className="form-row">
+          <label htmlFor="name" className="form-label">
             name
           </label>
-          <input type='text' className='form-input' id='name' />
+          <input
+            type="text"
+            className="form-input"
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
         </div>
 
-        <button type='submit' className='btn btn-block'>
+        <button type="submit" className="btn btn-block">
           submit
         </button>
       </form>
       {/* render users below */}
+      <h2>Users</h2>
+      {users.map((user) => {
+        return (
+          <div key={user.id}>
+            <h4>{user.name}</h4>
+          </div>
+        )
+      })}
     </div>
-  );
-};
-export default UserChallenge;
+  )
+}
+export default UserChallenge
