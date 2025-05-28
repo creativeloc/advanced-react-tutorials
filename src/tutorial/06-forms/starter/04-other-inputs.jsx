84e43e4bcd7e476b360bf7e25ee9b8a980a293bb
Lawrence Caulker <locaulker@gmail.com>
2025-05-27 23:52:37 -0400

    implement select functionality in react

--- src/tutorial/06-forms/starter/04-other-inputs.jsx
+++ src/tutorial/06-forms/starter/04-other-inputs.jsx
@@ -1,10 +1,17 @@
 import { useState } from "react"
 const frameworks = ["react", "angular", "vue", "svelte"]
+
 const OtherInputs = () => {
   const [shipping, setShipping] = useState(false)
+  const [framework, setFramework] = useState("react")
+
   const handleShipping = (e) => {
     console.log(e.target.checked)
     setShipping(e.target.checked)
+  }
+
+  const handleFramework = (e) => {
+    setFramework(e.target.value)
   }
 
   return (
@@ -26,6 +33,16 @@ const OtherInputs = () => {
           <label htmlFor="framework" className="form-label">
             Framework
           </label>
+          <select
+            name="framework"
+            id="framework"
+            value={framework}
+            onChange={handleFramework}
+          >
+            {frameworks.map((framework) => {
+              return <option key={framework}>{framework}</option>
+            })}
+          </select>
         </div>
         <button type="submit" className="btn btn-block">
           submit
