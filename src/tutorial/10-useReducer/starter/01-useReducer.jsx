3e9d39a52c47b074f529da8c83606fb67da761ee
creativeloc <creativeloc@yahoo.com>
2025-06-17 04:53:48 -0400

    setup basic useReducer() function

--- src/tutorial/10-useReducer/starter/01-useReducer.jsx
+++ src/tutorial/10-useReducer/starter/01-useReducer.jsx
@@ -1,25 +1,35 @@
-import { useState } from "react"
+import { useState, useReducer } from "react"
 import { data } from "../../../data"
 
+const defaultState = {
+  people: data
+}
+
+const reducer = () => {}
+
 const ReducerBasics = () => {
-  const [people, setPeople] = useState(data)
+  const [state, dispatch] = useReducer(reducer, defaultState)
+
+  // const [people, setPeople] = useState(data)
 
   const removeItem = (id) => {
-    let newPeople = people.filter((person) => person.id !== id)
-    setPeople(newPeople)
+    // let newPeople = people.filter((person) => person.id !== id)
+    // setPeople(newPeople)
   }
 
   const resetItems = () => {
-    setPeople(data)
+    // setPeople(data)
   }
 
   const clearItems = () => {
-    setPeople([])
+    // setPeople([])
   }
+
+  console.log(state)
 
   return (
     <div>
-      {people.map((person) => {
+      {state.people.map((person) => {
         const { id, name } = person
         return (
           <div key={id} className="item">
@@ -28,7 +38,7 @@ const ReducerBasics = () => {
           </div>
         )
       })}
-      {people.length > 1 ? (
+      {state.people.length > 1 ? (
         <button
           className="btn"
           style={{ marginTop: "2rem" }}
