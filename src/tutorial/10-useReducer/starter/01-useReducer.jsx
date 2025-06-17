bea41adc10eaf23eb51a4c54f846c380463efaa2
creativeloc <creativeloc@yahoo.com>
2025-06-17 00:44:33 -0400

    add reset button conditionally when data is empty

--- src/tutorial/10-useReducer/starter/01-useReducer.jsx
+++ src/tutorial/10-useReducer/starter/01-useReducer.jsx
@@ -1,32 +1,52 @@
-import React from 'react';
-import { data } from '../../../data';
+import { useState } from "react"
+import { data } from "../../../data"
+
 const ReducerBasics = () => {
-  const [people, setPeople] = React.useState(data);
+  const [people, setPeople] = useState(data)
 
   const removeItem = (id) => {
-    let newPeople = people.filter((person) => person.id !== id);
-    setPeople(newPeople);
-  };
+    let newPeople = people.filter((person) => person.id !== id)
+    setPeople(newPeople)
+  }
+
+  const resetItems = () => {
+    setPeople(data)
+  }
+
+  const clearItems = () => {
+    setPeople([])
+  }
+
   return (
     <div>
       {people.map((person) => {
-        const { id, name } = person;
+        const { id, name } = person
         return (
-          <div key={id} className='item'>
+          <div key={id} className="item">
             <h4>{name}</h4>
             <button onClick={() => removeItem(id)}>remove</button>
           </div>
-        );
+        )
       })}
-      <button
-        className='btn'
-        style={{ marginTop: '2rem' }}
-        onClick={() => setPeople([])}
-      >
-        clear items
-      </button>
+      {people.length > 1 ? (
+        <button
+          className="btn"
+          style={{ marginTop: "2rem" }}
+          onClick={clearItems}
+        >
+          clear items
+        </button>
+      ) : (
+        <button
+          className="btn"
+          style={{ marginTop: "2rem" }}
+          onClick={resetItems}
+        >
+          Reset Items
+        </button>
+      )}
     </div>
-  );
-};
+  )
+}
 
-export default ReducerBasics;
+export default ReducerBasics
